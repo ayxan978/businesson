c02f186f9757b02757a571402809a3116f6d5387
ayxan978 <zeuspro978@gmail.com>
2025-10-22 23:02:24 +0300

    second

---
+++ aiAdviceService/src/main/java/com/example/aiadviceservice/AiAdviceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.aiadviceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAdviceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiAdviceServiceApplication.class, args);
+    }
+
+}
---
+++ aiAdviceService/src/test/java/com/example/aiadviceservice/AiAdviceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiadviceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAdviceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
