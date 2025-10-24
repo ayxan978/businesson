d89243c36438f23d23ea3313a497229e4d154616
ayxan978 <zeuspro978@gmail.com>
2025-10-24 18:39:17 +0300

    B

---
+++ authService/src/main/java/com/example/authservice/dto/CompanyDtoRequest.java
@@ -0,0 +1,21 @@
+package com.example.authservice.dto;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CompanyDtoRequest {
+    String name;
+    String surname;
+    String password;
+    String email;
+    String phone;
+    String address;
+}
---
+++ authService/src/main/java/com/example/authservice/service/CompanyService.java
@@ -0,0 +1,9 @@
+package com.example.authservice.service;
+
+import com.example.authservice.dto.BossDtoRequest;
+import com.example.authservice.dto.BossDtoResponse;
+
+public interface CompanyService {
+    BossDtoResponse saveBoss(BossDtoRequest request);
+    BossDtoResponse updateBoss(BossDtoRequest request, Long id);
+}
